b8ad5763e2dd5d9930d564c608a0d6fbcabe6bcf
Shane Wilson <shane.wilson@oicr.on.ca>
2014-10-10 14:16:49 -0400

    chore(re): rel script stuff

---
+++ CHANGELOG.md
@@ -0,0 +1,10 @@
+changelog 
+
+changelog 1.4.2
+
+changelog 1.4.2
+
+changelog 1.4.2
+
+changelog 1.4.3
+
--- release.sh
+++ release.sh
@@ -1,16 +1,16 @@
 # usage
 # ./release.sh (release|retract) $COMMIT_SHA $NEXT_VERSION
 
-PACKAGE="package.json"
-SNAPSHOT="-SNAPSHOT"
-
-$COMMIT_SHA = $2
-NEXT_VERSION = $3${SNAPSHOT}
-
 # readJsonProp(jsonFile, property)
 # - restriction: property needs to be on a single line!
 function readJsonProp {
   echo $(sed -En 's/.*"'$2'"[ ]*:[ ]*"(.*)".*/\1/p' $1)
+}
+
+# replaceInFile(file, findPattern, replacePattern)
+function replaceInFile {
+  sed -i .tmp -E "s/$2/$3/" $1
+  rm $1.tmp
 }
 
 # replaceJsonProp(jsonFile, propertyRegex, valueRegex, replacePattern)
@@ -20,12 +20,20 @@ function replaceJsonProp {
   replaceInFile $1 '"('$2')"[ ]*:[ ]*"'$3'"' '"\1": "'$4'"'
 }
 
+PACKAGE="package.json"
+SNAPSHOT="-beta"
+
+CURRENT_VERSION=$(readJsonProp ${PACKAGE} "version")
+
+COMMIT_SHA=${2}
+NEXT_VERSION="${3}" # should be semver, should be greater than current_version
+
 # updates to release version
-RELEASE_VERSION=${$(readJsonProp ${PACKAGE} "version")//${SNAPSHOT}/}
+RELEASE_VERSION=${CURRENT_VERSION//${SNAPSHOT}/}
 replaceJsonProp ${PACKAGE} "version" ".*" ${RELEASE_VERSION}
 
 # updates changelog
-echo "changelog"
+echo "changelog ${RELEASE_VERSION}\n" >>  CHANGELOG.md
 
 # tag commit
 git add CHANGELOG.md package.json
@@ -33,7 +41,7 @@ git commit -m "chore(release): Release ${RELEASE_VERSION}"
 git tag -s ${RELEASE_VERSION} -m "chore(release): $RELEASE_VERSION" "$COMMIT_SHA"
 
 # updates to next development version
-replaceJsonProp ${PACKAGE} "version" ".*" ${NEXT_VERSION}
+replaceJsonProp ${PACKAGE} "version" ".*" ${NEXT_VERSION}${SNAPSHOT}
 
 # commit new version to master
 git add package.json
