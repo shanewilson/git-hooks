bbb031730081e41ff473589f488df8798dbc544c
Shane Wilson <shane.wilson@oicr.on.ca>
2014-10-10 16:27:03 -0400

    feat(build): pulls out some bash stuff to utils

--- gulpfile.js
+++ gulpfile.js
@@ -3,11 +3,8 @@ var fs = require('graceful-fs');
 
 gulp.task('changelog', function(){
   require('conventional-changelog')({
-    repository: 'https://github.com/NCI-GDC/portal-ui',
-    version: require('./package.json').version,
-    issueLink: function (id) {
-      return '[GDC-'+id+'](https://jira.oicr.on.ca/browse/GDC-' + id + ')'
-    }
+    repository: 'https://github.com/shanewilson/git-hooks/',
+    version: require('./package.json').version
   }, function(err, log) {
     fs.writeFile('CHANGELOG.md', log);
   });
--- release.sh
+++ release.sh
@@ -1,51 +1,100 @@
-# usage
-# ./release.sh (release|retract) $COMMIT_SHA $NEXT_VERSION
+#!/bin/sh
 
-# readJsonProp(jsonFile, property)
-# - restriction: property needs to be on a single line!
-function readJsonProp {
-  echo $(sed -En 's/.*"'$2'"[ ]*:[ ]*"(.*)".*/\1/p' $1)
+set -e
+
+source $(dirname $0)/utils.inc
+
+NEXT_VERSION_REGEX="^[0-9]+\.[0-9]+\.[0-9]+(\.[0-9]+)?$"
+ACTION_REGEX="^(prepare|publish|retract)$"
+COMMIT_SHA_REGEX="(.*)"
+ARG_DEFS=(
+	"--next-version=${NEXT_VERSION_REGEX}"
+	"[--action=${ACTION_REGEX}]"
+	"[--commit-sha=${COMMIT_SHA_REGEX}]"
+)
+
+while [[ $# > 1 ]]; do
+	key="$1"
+	shift
+
+	case $key in
+		-n|--next-version)
+	    NEXT_VERSION="$1"
+	    shift
+	    ;;
+	    -a|--action)
+	    ACTION="$1"
+	    shift
+	    ;;
+	    -c|--commit-sha)
+	    COMMIT_SHA="$1"
+	    shift
+	    ;;
+	    *)
+	    ;;
+	esac
+done
+
+if [[ -z ${NEXT_VERSION} || ! ${NEXT_VERSION} =~ ${NEXT_VERSION_REGEX} ||
+	  (! -z ${ACTION} && ! ${ACTION} =~ ${ACTION_REGEX}) ||
+	  (! -z ${COMMIT_SHA} && ! ${COMMIT_SHA} =~ ${COMMIT_SHA_REGEX}) ]]; then
+	usage
+fi
+
+compareVersions() {
+	printf "Checking that %s is greater than %s..." $(focus ${NEXT_VERSION}) $(focus ${RELEASE_VERSION})
+	if [[ ${NEXT_VERSION} > ${RELEASE_VERSION} ]]; then
+		echo_success
+	else
+		echo_failure "${ERR} The next version must be greater than the current version."
+	fi
 }
 
-# replaceInFile(file, findPattern, replacePattern)
-function replaceInFile {
-  sed -i .tmp -E "s/$2/$3/" $1
-  rm $1.tmp
+updateVersionToRelease() {
+	printf "Updating %s to %s in %s..." $(focus ${CURRENT_VERSION}) $(focus ${RELEASE_VERSION}) $(focus ${PACKAGE})
+	replaceJsonProp ${PACKAGE} "version" ".*" ${RELEASE_VERSION}
+	if [[ $(readJsonProp ${PACKAGE} "version") == ${RELEASE_VERSION} ]]; then
+		echo_success
+	else
+		echo_failure "${ERR} An error occurred while trying to update the development version."
+	fi
 }
 
-# replaceJsonProp(jsonFile, propertyRegex, valueRegex, replacePattern)
-# - note: propertyRegex will be automatically placed into a
-#   capturing group! -> all other groups start at index 2!
-function replaceJsonProp {
-  replaceInFile $1 '"('$2')"[ ]*:[ ]*"'$3'"' '"\1": "'$4'"'
+prepare() {
+	if [[ gitDirty ]]; then
+		echo "dirty"
+	else 
+		echo "not"
+	fi
+	exit
+	local PACKAGE="package.json"
+	local SNAPSHOT="-beta"
+
+	local CURRENT_VERSION=$(readJsonProp ${PACKAGE} "version")
+	local RELEASE_VERSION=${CURRENT_VERSION//${SNAPSHOT}/}
+
+	compareVersions
+	updateVersionToRelease
+
+	# updates changelog
+	npm run changelog
+
+	# tag commit
+	git add CHANGELOG.md package.json
+	git commit -m "chore(release): Release ${RELEASE_VERSION}"
+	git tag -s ${RELEASE_VERSION} -m "chore(release): $RELEASE_VERSION" "$COMMIT_SHA"
+
+	# updates to next development version
+	replaceJsonProp ${PACKAGE} "version" ".*" ${NEXT_VERSION}${SNAPSHOT}
+
+	# commit new version to master
+	git add package.json
+	git commit -m "chore(release): Start Development on ${NEXT_VERSION}"
 }
 
-PACKAGE="package.json"
-SNAPSHOT="-beta"
+publish() {
+	# push master and tags
+	git push origin master && git push origin $RELEASE_VERSION	
+}
 
-CURRENT_VERSION=$(readJsonProp ${PACKAGE} "version")
-
-COMMIT_SHA=${2}
-NEXT_VERSION="${3}" # should be semver, should be greater than current_version
-
-# updates to release version
-RELEASE_VERSION=${CURRENT_VERSION//${SNAPSHOT}/}
-replaceJsonProp ${PACKAGE} "version" ".*" ${RELEASE_VERSION}
-
-# updates changelog
-npm run changelog
-
-# tag commit
-git add CHANGELOG.md package.json
-git commit -m "chore(release): Release ${RELEASE_VERSION}"
-git tag -s ${RELEASE_VERSION} -m "chore(release): $RELEASE_VERSION" "$COMMIT_SHA"
-
-# updates to next development version
-replaceJsonProp ${PACKAGE} "version" ".*" ${NEXT_VERSION}${SNAPSHOT}
-
-# commit new version to master
-git add package.json
-git commit -m "chore(release): Start Development on ${NEXT_VERSION}"
-
-# push master and tags
-git push origin master && git push origin $RELEASE_VERSION
+prepare
