030da5d4dd60dd51289ba94386b3354004462e0e
Shane Wilson <shane.wilson@oicr.on.ca>
2014-10-14 16:26:25 -0400

    feat(release): better bash stuff

--- release.sh
+++ release.sh
@@ -1,14 +1,17 @@
-#!/bin/sh
+#!/bin/bash
 
-set -e
+set -o errexit
+set -o pipefail
 
 source $(dirname $0)/utils.inc
 
-NEXT_VERSION_REGEX="^[0-9]+\.[0-9]+\.[0-9]+(\.[0-9]+)?$"
-ACTION_REGEX="^(prepare|publish|retract)$"
-COMMIT_SHA_REGEX="(.*)"
-ARG_DEFS=(
-	"--next-version=${NEXT_VERSION_REGEX}"
+readonly PACKAGE="package.json"
+
+readonly VERSION_REGEX="^[0-9]+\.[0-9]+\.[0-9]+(\.[0-9]+)?$"
+readonly ACTION_REGEX="^(prepare|publish|next|retract)$"
+readonly COMMIT_SHA_REGEX="(.*)"
+readonly ARG_DEFS=(
+	"--version=${VERSION_REGEX}"
 	"[--action=${ACTION_REGEX}]"
 	"[--commit-sha=${COMMIT_SHA_REGEX}]"
 )
@@ -18,8 +21,8 @@ while [[ $# > 1 ]]; do
 	shift
 
 	case $key in
-		-n|--next-version)
-	    NEXT_VERSION="$1"
+		-v|--version)
+	    VERSION="$1"
 	    shift
 	    ;;
 	    -a|--action)
@@ -35,15 +38,26 @@ while [[ $# > 1 ]]; do
 	esac
 done
 
-if [[ -z ${NEXT_VERSION} || ! ${NEXT_VERSION} =~ ${NEXT_VERSION_REGEX} ||
+
+
+if [[ -z ${VERSION} || ! ${VERSION} =~ ${VERSION_REGEX} ||
 	  (! -z ${ACTION} && ! ${ACTION} =~ ${ACTION_REGEX}) ||
 	  (! -z ${COMMIT_SHA} && ! ${COMMIT_SHA} =~ ${COMMIT_SHA_REGEX}) ]]; then
 	usage
 fi
 
+set -o nounset
+
+readonly VERSION
+readonly ACTION=${ACTION:-"prepare"}
+readonly COMMIT_SHA=${COMMIT_SHA:-"HEAD"}
+
 compareVersions() {
-	printf "Checking that %s is greater than %s..." $(focus ${NEXT_VERSION}) $(focus ${RELEASE_VERSION})
-	if [[ ${2} > ${1} ]]; then
+	local FROM=$1
+	local TO=$2
+	
+	printf "Checking that %s is greater than %s..." $(focus ${FROM}) $(focus ${TO})
+	if [[ ${TO} -gt ${FROM} ]]; then
 		echo_success
 	else
 		echo_failure "The next version must be greater than the current version."
@@ -53,48 +67,71 @@ compareVersions() {
 updateVersion() {
 	local FROM=$1
 	local TO=$2
+	
+	compareVersions ${FROM} ${TO}
+
 	printf "Updating from %s to %s..." $(focus ${FROM}) $(focus ${TO})
-	replaceJsonProp ${PACKAGE} "version" ".*" ${TO}
-	if [[ $(readJsonProp ${PACKAGE} "version") == ${TO} ]]; then
-		echo_success
-	else
-		echo_failure "An error occurred while trying to update the development version."
+	if ! replaceJsonProp ${PACKAGE} "version" ".*" ${TO}; then
+		echo_failure "An error occurred while trying to update the development version."	
 	fi
+	echo_success
 }
 
 logs() {
 	./node_modules/gulp/bin/gulp.js logs
 }
 
+preChecks() {
+	printf "Preparing release for %s..." $(focus ${VERSION})
+	if git rev-parse ${VERSION}.. >/dev/null 2>&1; then
+		echo_failure "tag for ${VERSION} already exists!"
+	fi
+	echo_success
+
+	printf "Checking that there are no uncommited items..."
+	if [[ -n "$(git status --porcelain --ignore-submodules -unormal)" ]]; then
+		echo_failure "git is dirty! Please commit or stash your changes."
+	fi
+	echo_success
+
+	local CURRENT_VERSION=$(($(readJsonProp ${PACKAGE} "version")+0))
+
+	updateVersion ${CURRENT_VERSION} ${VERSION}
+}
+
 prepare() {
-	gitDirty
-
-	local PACKAGE="package.json"
-	local SNAPSHOT="-beta"
-
-	local CURRENT_VERSION=$(readJsonProp ${PACKAGE} "version")
-	local RELEASE_VERSION=${CURRENT_VERSION//${SNAPSHOT}/}
-
-	compareVersions ${RELEASE_VERSION} ${NEXT_VERSION} 
-	updateVersion ${CURRENT_VERSION} ${RELEASE_VERSION}
+	preChecks
 
 	# updates changelog
 	logs
-
+	
 	# tag commit
 	git add CHANGELOG.md package.json
-	git commit -m "chore(release): Release ${RELEASE_VERSION}"
-	git tag -s ${RELEASE_VERSION} -m "chore(release): $RELEASE_VERSION" "$COMMIT_SHA"
+	git commit -m "chore(release): Release ${VERSION}"
+	git tag -s ${VERSION} -m "chore(release): $VERSION" "$COMMIT_SHA" 	
+}
 
-	# updates to next development version
-	updateVersion ${RELEASE_VERSION} ${NEXT_VERSION}${SNAPSHOT}
+publish() {
+	git push origin $VERSION
+}
+
+next() {
+	preChecks
 
 	# commit new version to master
 	git add package.json
-	git commit -m "chore(release): Start Development on ${NEXT_VERSION}"
-
-	# push master and tags
-	git push origin master && git push origin $RELEASE_VERSION	
+	git commit -m "chore(release): Start Development on ${VERSION}"
 }
 
-prepare
+retract() {
+	printf "This will remove %s and %s copies of %s\n" $(focus "local") $(focus "remote") $(focus ${VERSION})
+	printf "If you wish to continue re-enter the tag name: "
+	local IFS= read -r TAG
+	
+	if [[ ${TAG} -eq ${VERSION} ]]; then
+		git tag -d ${VERSION}
+		git push origin :refs/tags/${VERSION}	
+	fi
+}
+
+${ACTION}
